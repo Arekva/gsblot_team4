795669ad0cd6a58465290d246e6be98a59a32e61
Arthur <tutur716@gmail.com>
2020-09-04 17:01:00 +0200

    Page login (front)

---
+++ src/view/login.php
@@ -0,0 +1,17 @@
+<div class="centrermoi">
+<form class="form">
+  <div class="log-img">
+    <img src="includes/logoGSB.png">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Identifiant:</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entrez identifiant">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Mot de passe:</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Se connecter</button>
+</form>
+</div>
