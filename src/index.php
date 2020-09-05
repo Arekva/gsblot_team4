8543605e34f97b40b2597564cc972f1a22889f57
YobiNoShio <teto1306@gmail.com>
2020-09-05 12:36:56 +0200

    test

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ date_default_timezone_set('Europe/Paris');
 /*include "connexion.php" ;*/
 
 include "view/header.php";
-include "controller/c_accueil.php";
+include "controller/c_home.php";
 include "view/footer.php";
 
 ?>
