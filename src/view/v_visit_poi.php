90d12e1c769cb91bf9c0caefcb310459393af6c6
Theo Lecoublet <53373927+YobiNoShio@users.noreply.github.com>
2020-09-18 17:28:51 +0200

    add front view visit poi

--- src/view/v_visit_poi.php
+++ src/view/v_visit_poi.php
@@ -1,5 +1,72 @@
 <!--piece of information-->
 <!--date and doctor's information-->
-<div class="temp">
-    
-</div>
+
+
+
+
+
+
+<form class="form" method="Post"  action="index.php?action=authentification">
+
+
+    <div class="form-group">
+        <label for="inputState">Selectionnez médecin</label>
+        <select id="inputState" class="form-control">
+            <option selected>- Médecins -</option>
+            <option>Médecins Numéro1</option>
+            <option>Médecins Numéro2</option>
+            <option>Médecins Numéro3</option>
+            <option>Médecins Numéro4</option>
+      </select>
+    </div>
+
+    <div class="form-group">
+        <label for="date">Insérer date</label>
+        <input type="date" class="form-control" aria-describedby="date" placeholder="" name= "date">
+    </div>
+
+    <div class="form-group">
+        <label for="inputState">Selectionnez médicament</label>
+        <select id="inputState" class="form-control">
+            <option selected>- Médicament -</option>
+            <option>Médicament Numéro1</option>
+            <option>Médicament Numéro2</option>
+            <option>Médicament Numéro3</option>
+            <option>Médicament Numéro4</option>
+      </select>
+    </div>
+
+  <button type="submit" class="btn btn-primary">Valider</button>
+</form>
+<form class="form" method="Post"  action="index.php?action=authentification">
+
+
+    <div class="form-group">
+        <label for="inputState">Selectionnez médecin</label>
+        <select id="inputState" class="form-control">
+            <option selected>- Médecins -</option>
+            <option>Médecins Numéro1</option>
+            <option>Médecins Numéro2</option>
+            <option>Médecins Numéro3</option>
+            <option>Médecins Numéro4</option>
+      </select>
+    </div>
+
+    <div class="form-group">
+        <label for="date">Insérer date</label>
+        <input type="date" class="form-control" aria-describedby="date" placeholder="" name= "date">
+    </div>
+
+    <div class="form-group">
+        <label for="inputState">Selectionnez médicament</label>
+        <select id="inputState" class="form-control">
+            <option selected>- Médicament -</option>
+            <option>Médicament Numéro1</option>
+            <option>Médicament Numéro2</option>
+            <option>Médicament Numéro3</option>
+            <option>Médicament Numéro4</option>
+      </select>
+    </div>
+
+  <button type="submit" class="btn btn-primary">Valider</button>
+</form>
